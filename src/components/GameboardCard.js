69ae570e4d3c365703a2fc2d510787e6fe57c79a
David Matalik <david.matalik@posteo.de>
2021-07-01 10:39:48 +0200

    Implement counter for GameboardCard

--- src/components/GameboardCard.js
+++ src/components/GameboardCard.js
@@ -1,14 +1,25 @@
 import styled from 'styled-components'
+import { useState } from 'react'
 
 const Card = styled.div`
   order: ${(props) => props.order};
 `
 
 const GameboardCard = (props) => {
+  const [counter, setCounter] = useState(0)
   const { order, onclickShuffle, title } = props
 
+  const onclickIncrementCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  const runClickFunctions = () => {
+    onclickShuffle()
+    onclickIncrementCounter()
+  }
+
   return (
-    <Card order={order} onClick={onclickShuffle}>
+    <Card order={order} onClick={runClickFunctions}>
       {title}
     </Card>
   )

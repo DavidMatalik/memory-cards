9f4fd35d06d37cdd14144deec1135e93bdfe5013
David Matalik <david.matalik@posteo.de>
2021-08-09 09:07:15 +0200

    Implement bestScore functionality

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,10 +1,27 @@
-import ScoreboardScore from './ScoreboardScore'
+import { useState, useEffect } from 'react'
 
 const Scoreboard = (props) => {
   const { resetCards, cardClick } = props
+  const [bestScore, setBestScore] = useState(0)
+  const [score, setScore] = useState(0)
+
+  useEffect(() => {
+    if (cardClick) {
+      setScore(score + 1)
+    }
+  }, [cardClick])
+
+  useEffect(() => {
+    if (score > bestScore) {
+      setBestScore(score)
+    }
+    setScore(0)
+  }, [resetCards])
+
   return (
     <div>
-      <ScoreboardScore resetCards={resetCards} cardClick={cardClick} />
+      <div>Score: {score} </div>
+      <div>Best score: {bestScore}</div>
     </div>
   )
 }
